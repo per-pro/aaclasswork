2247e8e31f11d5ce2539f53e6e3bd46d6a1a67f8
Itai Farhi <itaimf@gmail.com>
2020-12-07 14:13:16 -0800

    add new_move_positions

--- W4D1/KnightPathFinder.rb
+++ W4D1/KnightPathFinder.rb
@@ -11,20 +11,38 @@ class KnightPathFinder
         if (x + 1 > 7 || y + 1 > 7  || x+ 2 > 7 || y + 2 > 7 ) 
             raise "invalid move"
         end
+        
         if (x - 1 < 0 || y - 1 < 0  || x- 2 < 0 || y - 2 < 0 ) 
             raise "invalid move"
         end
-        x += 1
-    #    possible =[ [1,2],[2,1]]
-        possible =  []
-        possible << [x+]
-        
 
-        
+        possible =  []
+        possible << [x + 1, y + 2]
+        possible << [x + 1, y - 2]
+        possible << [x + 2, y + 1]
+        possible << [x + 2, y - 1]
+        possible << [x - 1, y + 2]
+        possible << [x - 1, y - 2]
+        possible << [x - 2, y + 1]
+        possible << [x - 2, y - 1]
+        possible
+    end
+    
+    def new_move_positions(pos)
+        new_pos = []
+        self.valid_moves(pos).each do |arr|
+            if !@considered_positions.include?(arr)
+                @considered_positions << arr
+                new_pos << arr 
+            end
+        end
+        new_pos
     end
 
-    def new_move_positions(pos)
-
+    def build_move_tree(position)
+        unless position.empty? position.each do |node|
+             node.bfs()
+        end
     end
 
     
