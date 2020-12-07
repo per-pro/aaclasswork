fe8943ddce9dd1f27bb1638c0c7aabfb05a02250
your name here <rupesh_singh87@yahoo.com>
2020-12-07 15:33:25 -0600

    Valid moves

--- W4D1/KnightPathFinder.rb
+++ W4D1/KnightPathFinder.rb
@@ -8,6 +8,14 @@ class KnightPathFinder
 
     def self.valid_moves(pos)
         x, y = pos[0], pos[1]
+        if (x + 1 > 7 || y + 1 > 7  || x+ 2 > 7 || y + 2 > 7 ) 
+            raise "invalid move"
+        end
+        if (x - 1 < 0 || y - 1 < 0  || x- 2 < 0 || y - 2 < 0 ) 
+            raise "invalid move"
+        end
+        
+
         
     end
 
--- W4D1/skeleton/lib/00_tree_node.rb
+++ W4D1/skeleton/lib/00_tree_node.rb
@@ -1,7 +1,9 @@
 require "byebug"
 
 class PolyTreeNode
+    include Searchable
     attr_reader :value, :parent, :children
+    attr
     def initialize(value)
         @value = value
         @parent = nil
@@ -34,36 +36,43 @@ class PolyTreeNode
         raise "node is not a child" if node.parent == nil
         node.parent = nil
     end
-
+end
     #  debugger
     # check the root, if it is equal to value return it
     # call dfs on the children of root 
     # return nil if not found
-    def dfs(target)
-        # debugger
-        return nil if self.value.nil?
-        return self if target == self.value
-        self.children.each do |child|
-            search_result = child.dfs(target)
-            return search_result unless child.nil?
-        end
-        nil
-    end
 
-    def bfs(target)
-        return self if self.value == target
-        arr = []
-        arr << self.children
-        self.children.each do |node|
-            arr << node.children
-            if node.value == target
-                return node
-            else
-                arr.pop
-            end
-        end
-        nil
-    end
-
+module Searchable
+  # I wrote this as a mixin in case I wanted to later write another
+  # TreeNode class (e.g., BinaryTreeNode). All I need is `#children` and
+  # `#value` for `Searchable` to work.
+  def dfs(target = nil, &prc)
+    raise "Need a proc or target" if [target, prc].none?
+    prc ||= Proc.new { |node| node.value == target }
     
+      return self if prc.call(self)
+
+    children.each do |child|
+      result = child.dfs(&prc)
+      return result unless result.nil?
+    end
+    nil
+  end
+  def bfs(target = nil, &prc)
+    raise "Need a proc or target" if [target, prc].none?
+    prc ||= Proc.new { |node| node.value == target }
+
+    nodes = [self]
+    until nodes.empty?
+      node = nodes.shift
+
+      return node if prc.call(node)
+      nodes.concat(node.children)
+    end
+    nil
+  end
+
+  def count
+    1 + children.map(&:count).inject(:+)
+  end
 end
