c0e54655f3ef7fcf295fd603cd276cd2ac9987c7
appkenny <kzeng.95@gmail.com>
2020-12-10 14:13:55 -0800

    Coninuted with my_uniq

--- W4D4/lib/tdd.rb
+++ W4D4/lib/tdd.rb
@@ -1,7 +1,12 @@
 class Array
 
     def my_uniq
-        
+        # uniq_arr = []
+        # self.each do |ele|
+        #     uniq_arr << ele unless uniq_arr.include?(ele)
+        # end
+        # uniq_arr
+        self.uniq
     end
 
 end
--- W4D4/spec/tdd_spec.rb
+++ W4D4/spec/tdd_spec.rb
@@ -3,11 +3,23 @@ require "tdd"
 
 
 describe Array do 
-    subject(:arr) {Array.new}
+    subject(:arr) {[1, 2, 1, 3, 3]}
     describe "Array#my_uniq" do 
         context "validate input" do
             it "self must be an array" do 
                 expect(arr.is_a?(Array)).to be true
+            end
+        end
+        #subject.uniq == [1, 2, 3]
+        describe "correctly finds unique elements" do
+            it "finds unique elements" do
+                expect(arr.my_uniq).to eq([1, 2, 3])
+            end
+        end
+        #dont want method to use built-in .uniq method
+        describe "doesn't rely on .uniq method" do
+            it "doesn't use .uniq" do
+                expect(arr).to_not receive(:uniq).with(arr)
             end
         end
     end
