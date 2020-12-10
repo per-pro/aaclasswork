27b1eaf3e9b3421ed8001ebcde3915d112113c1f
Itai Farhi <itaimf@gmail.com>
2020-12-10 14:55:11 -0800

    fixed my_uniq and worked on two_sum

--- W4D4/lib/tdd.rb
+++ W4D4/lib/tdd.rb
@@ -1,12 +1,25 @@
 class Array
 
     def my_uniq
-        # uniq_arr = []
-        # self.each do |ele|
-        #     uniq_arr << ele unless uniq_arr.include?(ele)
+        uniq_arr = []
+        self.each do |ele|
+            uniq_arr << ele unless uniq_arr.include?(ele)
+        end
+        uniq_arr
+        # self.uniq
+    end
+
+    def two_sum
+        # hash = Hash.new(0)
+        # arr = []
+        # self.each_with_index do |n, i|
+        #     if hash.has_key?(-n)
+        #         arr << [hash[n], i]
+        #     else
+        #         hash[n] = i
+        #     end
         # end
-        # uniq_arr
-        self.uniq
+        # arr 
     end
 
 end
--- W4D4/spec/tdd_spec.rb
+++ W4D4/spec/tdd_spec.rb
@@ -3,8 +3,8 @@ require "tdd"
 
 
 describe Array do 
-    subject(:arr) {[1, 2, 1, 3, 3]}
     describe "Array#my_uniq" do 
+        subject(:arr) {[1, 2, 1, 3, 3]}
         context "validate input" do
             it "self must be an array" do 
                 expect(arr.is_a?(Array)).to be true
@@ -19,8 +19,22 @@ describe Array do
         #dont want method to use built-in .uniq method
         describe "doesn't rely on .uniq method" do
             it "doesn't use .uniq" do
-                expect(arr).to_not receive(:uniq).with(arr)
+                expect(arr).to_not receive(:uniq)
+                arr.my_uniq
             end
         end
+    end
+
+    describe "Array#two_sum" do 
+        subject(:arr) {[-1, 0, 2, -2 , 1]}
+        context "valid output" do
+            it "gets correct output" do 
+                expect(arr.two_sum).to eq([[0, 4], [2, 3]])
+            end
+            it "gets correct output" do 
+                expect(arr.two_sum).to_not eq([[4, 0], [3, 2]])
+            end
+        end        
+
     end
 end
