1bcfcec2158b4c77c911a71f5a62c0226a479b4b
Fabio R Bortone <maybe.fabio@gmail.com>
2020-12-11 14:02:57 -0800

    change largest_contiguous_subsum to linear time

--- W4D5/execution_time_diff.rb
+++ W4D5/execution_time_diff.rb
@@ -10,44 +10,46 @@ end
 # p my_min(list)  # =>  -5
 
 # def largest_contiguous_subsum(list)
-#     # arr = []
-#     # x = 0
-#     # while x < list.length
-#     #     # debugger
-#     #     y = x 
-#     #     while y < list.length 
-#     #         arr << list[x..y]
-#     #         y += 1
-#     #     end
-#     #     x += 1
-#     # end
-#     # max_subsum = arr.first.sum
-#     # arr.each {|subarr| max_subsum = subarr.sum if subarr.sum > max_subsum}
-#     # max_subsum
-# end
-
-# def largest_contiguous_subsum(list)
-#     hash = Hash.new(0)
-#     overall_sum = 0
-#     until list.empty?
-#         current_sum = list.sum
-#         overall_sum = current_sum if current_sum > overall_sum
-        
-#         list.shift
+#     arr = []
+#     x = 0
+#     while x < list.length
+#         # debugger
+#         y = x 
+#         while y < list.length 
+#             arr << list[x..y]
+#             y += 1
+#         end
+#         x += 1
 #     end
-#     overall_sum
+#     max_subsum = arr.first.sum
+#     arr.each {|subarr| max_subsum = subarr.sum if subarr.sum > max_subsum}
+#     max_subsum
 # end
 
-# list = [2, 3, -6, 7, -6, 7]
+def largest_contiguous_subsum(list)
+    overall_sum = 0
+    until list.empty?
+        current_sum = list.sum
+        overall_sum = current_sum if current_sum > overall_sum
+        
+        list.shift
+    end
+    overall_sum
+end
+
+list = [2, 3, -6, 7, -6, 7]
 # p largest_contiguous_subsum(list) # => 8 (from [7, -6, 7])
 
 def largest_contiguous_subsum(list)
-    hash = Hash.new(0)
-    until list.empty?
-        hash[list.length] = list.sum
-        list.shift
+    helper_arr = [list.first]
+    (1...list.length).each do |idx|
+        if helper_arr[-1] + list[idx] > list[idx]
+            helper_arr << helper_arr[-1] + list[idx]
+        else
+            helper_arr << list[idx]
+        end
     end
-    hash.values.max
+    helper_arr.max
 end
 
 list = [2, 3, -6, 7, -6, 7]
