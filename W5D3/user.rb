e0cbd56466901c40af94a748f6a6c27d417e3106
Itai Farhi <itaimf@gmail.com>
2020-12-16 15:02:25 -0800

    finish find by id for user class

---
+++ W5D3/user.rb
@@ -0,0 +1,24 @@
+require_relative "database"
+
+class User
+    attr_accessor :id, :fname, :lname 
+
+    def self.find_by_id(id)
+        raise "#{self} not in database" unless self.id
+        person = QuestionsDatabase.instance.execute(<<-SQL, id)
+            SELECT *
+            FROM users
+            WHERE id = ?
+        SQL
+        return nil unless person.length > 0
+        User.new(person)
+    end
+
+    def initialize(options)
+        @id = options["id"]
+        @fname = options["fname"]
+        @lname = options["lname"]
+    end
+
+
+end
