d39b42875083b2613cc3df8407d4de167610b930
Itai Farhi <itaimf@gmail.com>
2020-12-16 14:20:38 -0800

    Create tables

---
+++ W5D3/import_db.sql
@@ -0,0 +1,58 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    fname TEXT NOT NULL,
+    lname TEXT NOT NULL
+)
+
+DROP TABLE IF EXISTS questions;
+CREATE TABLE questions (
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL, 
+    FOREIGN KEY (user_id) REFERENCES users(id)
+)
+
+DROP TABLE IF EXISTS question_follows;
+CREATE TABLE question_follows (
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL, 
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id)
+)
+
+DROP TABLE IF EXISTS replies;
+CREATE TABLE replies (
+    id INTEGER PRIMARY KEY,
+    body TEXT NOT NULL,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    reply_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+    FOREIGN KEY (reply_id) REFERENCES replies(id)
+)
+
+DROP TABLE IF EXISTS question_likes;
+CREATE TABLE question_likes (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    question_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (question_id) REFERENCES questions(id),
+)
+
+INSERT INTO 
+    users(fname, lname)
+VALUES
+    ("Colin", "Eckert")
+    ("Itai", "Farhi")
+
+INSERT INTO
+    questions(title, body, user_id)
+VALUES
+    ("What OS is better?", "Thinking about buying a new computer", 
+    (SELECT id FROM users WHERE fname = "Colin" AND lname = "Eckert"))
