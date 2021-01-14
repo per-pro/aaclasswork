b119216858e7f4fca924af6277221e25f947fb8d
IsoVoyd <dubnersamuel@gmail.com>
2021-01-13 16:46:23 -0800

    continue working on phase 4

--- W8D3/skeleton(6)/skeleton/phase_4_recursion.js
+++ W8D3/skeleton(6)/skeleton/phase_4_recursion.js
@@ -14,4 +14,35 @@ let sumRec = function(arr) {
     return arr[0] + sumRec(arr.slice(1,arr.length))
 }
 
-let exponent = function(base)
+let exponentA = function(base, exponent) {
+    if (exponent === 0) {
+        return 1
+    }
+    return base * exponentA(base, exponent - 1)
+}
+
+let exponentB = function(base, exponent) {
+    if (exponent === 0) {
+        return 1
+    } else if (exponent === 1) {
+        return base
+    } 
+    if (exponent % 2 === 0) {
+        return exponentB(base, exponent / 2) ** 2
+    } else {
+        return base * (exponentB(base, (exponent - 1)/2) ** 2)
+    }
+}
+
+let fib = function(int) {
+    fibs = []
+    if (int === 1) {
+        fibs = [1]
+        return fibs
+    } else if (int === 2) {
+        fibs = [1, 1]
+        return fibs
+    }
+    let nextFib = fibs[fibs.length - 1] + fibs[fibs.length - 2]
+    return fibs
+}
